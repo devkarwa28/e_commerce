c0be3e4e6b86a346f279d64b37928d1150aec31e
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-02-23 23:52:50 +0530

    Slick Slider Implented

--- client/app/layout.js
+++ client/app/layout.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@emotion/react";
 import theme from "@/theme/theme";
 import { CssBaseline } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 export default function RootLayout({ children }) {
--- client/components/Home/HeroBanner.jsx
+++ client/components/Home/HeroBanner.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import homestyles from './home.module.css'
+
+
 const HeroBanner = () => {
+    const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
     return (
         <section className={homestyles.heroBanner}>
             <div className="container">
@@ -8,18 +20,18 @@ const HeroBanner = () => {
                     <div className="col-lg-6">
                         <div className={homestyles.heroContent}>
                             <h5 className='px-3 py-1 mb-2 d-inline-block'>UP TO 40% OFF</h5>
-                            
                             <h1>Premium Dry Fruits <br />For Every Occasion</h1>
                             <p>
                                 Naturally sourced, freshly packed and delivered
                                 with care. Experience the finest quality nuts,
                                 berries and seeds.
                             </p>
-
                         </div>
                     </div>
                     <div className="col-lg-6">
+                        <div className='slider'>
 
+                        </div>
                     </div>
                 </div>
             </div>
