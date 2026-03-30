0c2278e0d181215095cdb458677046c045d547b3
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-03-30 20:36:32 +0530

    Token Generator

--- server/utilites/tokenGenarator.js
+++ server/utilites/tokenGenarator.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const tokenGenarator = (res, userId) =>{
-    const token = jwt.sign({id: userId},"devkarwa@2004",{expiresIn: '7d'});
+    const token = jwt.sign({id: userId},process.env.JWT_SECRET,{expiresIn: '7d'});
 
     res.cookie("token",token,{
         httpOnly : true,
-        secure: false,
-        sameSite : "lax",
+        secure: true,
+        sameSite : "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
