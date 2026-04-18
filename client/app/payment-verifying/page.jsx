835bbff27f7fc0061dee7e875a63f1b967fb6380
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-04-18 13:54:12 +0530

    Update page.jsx

--- client/app/payment-verifying/page.jsx
+++ client/app/payment-verifying/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import SecurityRoundedIcon from "@mui/icons-material/SecurityRounded";
