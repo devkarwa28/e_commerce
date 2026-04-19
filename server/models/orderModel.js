b19307e4526f532d6365dc865296819d3a0d7bbb
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-04-19 13:34:19 +0530

    Update orderModel.js

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -75,7 +75,7 @@ const orderSchema = new mongoose.Schema({
 
     paymentMethod: {
         type: String,
-        enum: ["COD", "Razorpay"],   
+        enum: ["COD", "Online"],   
         default: "COD",
     },
     paymentStatus: {
