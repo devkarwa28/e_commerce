c98a30e108e13146e09e2b1cb25ca59d1c68de61
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2026-04-29 20:12:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: ".env.local" });
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -62,7 +62,7 @@ app.use("/api/coupons", couponRouter);
 app.use("/api/banners", bannerRouter);
 app.use("/api/wishlist", wishRouter);
 app.use("/api/payment", paymentRouter);
-
-app.listen(5000, () => {
-  console.log("Server Started on PORT 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server Started on PORT ${PORT}`);
 });
